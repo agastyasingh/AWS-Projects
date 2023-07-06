0afb198d15e520c4d0727a73c9982e3ff2b56d0f
agastyasingh <agastyasingh927@gmail.com>
2023-07-06 19:20:08 +0530

    First Commit

---
+++ AWS Step Function/processPurchase.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+import datetime
+
+print('Loading function...')
+
+def process_purchase(message, context):
+
+    print("Recieved message from step Functions:")
+    print(message)
+
+    response = {}
+    response['TransactionType'] = message['TransactionType']
+    response['Message'] = 'Hello from lambda land inside the ProcessPurchase function'
+
+    return response
